541d9b8a544b3a471917c668c151dbb059b54bb6
ilzoso <ilzoso@github.com>
2017-03-08 22:33:55 -0800

    Initial commit

---
+++ main.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import HelloWorld from './HelloWorld.jsx';
+
+ReactDom.render(
+  <HelloWorld />,
+  document.getElementById('container')
+);
+
+window.addEventListener('click', function(e) {
+  if (e.target.tagName === 'A' && e.target.getAttribute('href') === '#') {
+    e.preventDefault();
+  }
+});
+
+console.error = (function(old) {
+  return function error() {
+    errors.textContent += Array.prototype.slice.call(arguments).join(' ') + '\n';
+    errors.style.display = '';
+    old.apply(this, arguments);
+  }
+})(console.error);
+
+if (window && window.Worker) {
+}
+
+
+
---
+++ modules/HelloWorld.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+class HelloWorld extends React.Component {
+    constructor() {
+    }
+    render() {
+        return <div>Hello World!!!</div>;
+    }
+}
+
+export default HelloWorld;
